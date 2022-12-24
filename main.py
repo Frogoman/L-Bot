dc48ee36616d210e200bf5729be0af859dd27642
Frogoman <carlossb23@gmail.com>
2022-12-24 05:57:38 +0100

    Removed debug line

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ async def on_message(message):
     if message.content.startswith('!test'):
         if "frry" in [role.name.lower() for role in message.author.roles]:
             await message.channel.send("Yes")
-            print("Yes")
         else:
             await message.channel.send("No")
             print(getLocalTime(), "       ", message.author.name , " Has tried to access the command '!test'")
