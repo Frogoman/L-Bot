82985ee23edbc0e2058caba72f29b0fd99dfc75a
Sukuratchi <scratch@boba.best>
2023-01-01 03:32:13 +0300

    Changed the way the bot grabs text. Now it uses a text file and makes the main file more clean

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # L Bot
  Pokatto made L bot
+
+ To add phrases, add a line to one of the text files and make a pull request!
--- main.py
+++ main.py
@@ -9,59 +9,6 @@ token = os.getenv('TOKEN')
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 
-Responses_Squ = [
-    "You are making me jealous...",
-    "Did you know that she is a cat?",
-    "Why are you talking to her?",
-    "She knows everything...",
-    "Stop! No touchy the space cat!",
-    "Bubble tea! Hah! Why are you looking at me like that...",
-    "Psst! She is busy!",
-    "You know she can hear you, right?",
-    "Her tail is so fluffy!!",
-    "You know if you talk to her too much, I will tentacle you.",
-    "Don't look at me... but we are out of coffee.",
-    "I don't like that look in your eyes! Why do you need her?",
-    "She will eat your face!"
-]
-
-Responses_Coffee = [
-    "Give me",
-    "I want it"
-]
-
-Responses_L = [
-    "Music starto....",
-    "Ĭ̴̈́ ̷̈́̌s̷̏̋e̶̐̿ȇ̶͑ ̶͇̏è̸̚v̶̇́ẹ̷̌r̷̊̓y̷̏́t̴̋̕ḧ̶͆ì̷̕n̵̪͝g̷͐̀",
-    "I dont understand",
-    "I don't like the moon. It's lonely.",
-    "I am not really an octocat.",
-    "I don't eat humans! Shy won't let me!",
-    "I hunger.",
-    "Why does everyone want to touch my tentacle?",
-    "I dont understand",
-    "Do you have a snack?",
-    "No... I don't need an eyedrop.",
-    "Earth is a strange place.",
-    "I can look like anything... but too much of me scares people.",
-    "Plants are beautiful... but Shy forgets to water them all the time.",
-    "Can I help?",
-    "I like to touch the coffee.",
-    "Why are you persistent?",
-    "She is way too into spooky stories... should I look scarier?",
-    "I ate some spaghetti. It was nice and crunchy!",
-    "Oh Nyo~",
-    "I like watching movies! You can rewatch people move over and over and over and over... ah...",
-    "Shy's hair smells nice, so I ate her shampoo. I hiccuped bubbles for a week!",
-    "I don't feel homesick anymore. I am home.",
-    "We don't actually have facehuggers on the station... at least I don't think so.",
-    "-Hiccup- I ate a block of soap.",
-    "Humans are so fragile...",
-    "Never gonna give you up! Never gonna let you down! Never gonna let you touch my space cat...",
-    "I did eat her safety skirt. I have no regrets. Please don't tell her...",
-    "My name is Alheitrakzoth."
-]
-
 @client.event
 async def on_ready():
     print("{0.user} is online".format(client))
@@ -75,11 +22,17 @@ async def on_message(message):
     user_message = str(message.content.strip())
 
     if user_message.lower().startswith("squ ") or user_message.lower().startswith("squchan ") or user_message.lower() == "squ" or user_message.lower() == "squchan":
+        with open("responses-l.txt",encoding="utf8") as f:
+            Responses_Squ = f.readlines()
         await message.channel.send(random.choice(Responses_Squ))
     elif user_message.lower().startswith("l ") or user_message.lower() == "l":
-        await message.channel.send(random.choice(Responses_L))
+        with open("responses-l.txt",encoding="utf8") as f:
+            L_Response = f.readlines()
+        await message.channel.send(random.choice(L_Response))
     elif user_message.lower().startswith("coffee ") or user_message.lower() == "coffee":
+        with open("responses-coffee.txt",encoding="utf8") as f:
+            Responses_Coffee = f.readlines()
         await message.channel.send(random.choice(Responses_Coffee))
 
 
-client.run(token)
+client.run(token)   
